c106eaccdc6e5cf8fecdd37bf892e6bab358a8bf
browningjp <j.p.browning@durham.ac.uk>
2019-08-10 17:26:51 +0100

    Initial release

---
+++ oscar_listings_shortcodes.php
@@ -0,0 +1,169 @@
+<?php
+/*
+Plugin Name: Oscar Listings Shortcodes
+Description: Use WordPress shortcodes to create listings pages for shows using the Oscar booking system by Savoy Systems Ltd. Please note that this project is not affiliated with Savoy Systems Ltd.
+Version: 1.0
+Author: Jonny Browning
+Author URI: https://www.jonnybrowning.com
+*/
+
+// Register shortcode
+add_shortcode("oscar-listings", "oscarlistings_function");
+
+function oscarlistings_function($shortcodeParams) {
+
+  // get Oscar listings
+  $feed_url = get_option('oscarlistings_feedURL');
+  $shows = fetch_listings($feed_url);
+
+	$listingsOutput = "";
+  // filter if DST / AR / SR depending on shortcode attributes
+  foreach($shows as $show) {
+    // if there are no parameters in the shortcode, output all shows
+    if (!$shortcodeParams) {
+      $listingsOutput .= output_listing($show);
+    }
+    // for each parameter in the shortcode, if true check to see if value is true in each show
+    foreach($shortcodeParams as $key=>$shortcodeParam) {
+      if($shortcodeParam and $show['@attributes'][$key] == "Y") {
+        $listingsOutput .= output_listing($show);
+      }
+    }
+  }
+
+  // if no shows, add a note to the output
+  if ($listingsOutput == "") {
+    $listingsOutput = "<p class='oscar-listing-no-shows'>There are no upcoming shows in the calendar. Please check back soon!</p>";
+  }
+
+  return $listingsOutput;
+}
+
+// generate output for a show listing and return it as a string
+function output_listing($show) {
+  $listingOutput = "";
+  $listingOutput .= "<div class='oscar-listing-outer-container'>";
+  $listingOutput .=
+    "<div class='oscar-listing-title'>
+      <a href=" . $show['@attributes']['BookingURL'] . "><h1>" . $show['@attributes']['Title'] . "</h1></a>
+    </div>
+    <div class='oscar-listing-inner-container'>
+      <div class='oscar-listing-image'>
+        <a href=" . $show['@attributes']['BookingURL'] . ">
+          <img src='" . $show['@attributes']['InternetBookingImageURL'] . "' alt='" . $show['@attributes']['Title'] . "' />
+        </a>
+      </div>
+      <div class='oscar-listing-synopsis'>"  . $show['@attributes']['Synopsis'] ."</div>
+    </div>";
+    $listingOutput .=
+    "<div class='oscar-listing-performances-container'>
+      <h2>Book tickets</h2>
+      <table class='oscar-listing-performances-table'>";
+
+    // list performances
+    foreach($show['performances'] as $performance) {
+      if($performance['HideFromInternetSales'] == "N") { // don't show performance if HideFromInternetSales API flag = "Y"
+        $listingOutput .= "<tr>";
+        $listingOutput .=
+        "<td>" . date("l jS F Y",strtotime($performance['StartDate'])) . "</td>
+        <td>" . date("g.ia",strtotime($performance['StartTime'])) . "</td>";
+        $listingOutput .= "<td>";
+        if ($performance['IsOpenForSale'] == "Y" and $performance['AllowInternetSales'] ==  "Y") { // only show performances if allowed by API flags
+          $listingOutput .= "<a href='" . $performance['BookingURL'] . "'>Book tickets</a>";
+        } else if ($performance['IsOpenForSale'] == "N") {
+          $listingOutput .= "Tickets not currently on sale";
+        } else {
+          $listingOutput .= "Online booking unavailable - please contact the box office";
+        }
+        $listingOutput .= "</td>
+        </tr>";
+      }
+    }
+
+    $listingOutput .=
+      "</table>
+    </div>
+  </div>
+  ";
+  return $listingOutput;
+}
+
+function fetch_listings($feed_url) {
+
+  // Fetch shows from Oscar
+  $response = wp_remote_get($feed_url);
+  $body = wp_remote_retrieve_body($response);
+
+  // convert result to XML object
+  $xml  = simplexml_load_string($body);
+
+  // Add programmes to array
+  $programmes = array();
+  foreach ($xml->Programme as $programme) {
+    $programme2 = (array) (object) $programme;
+    $performances = (array) $xml->xpath('//Performance[@ProgrammeID="' . $programme['ID'] . '"]'); // get all performances associated with this programme;
+    usort($performances, 'comparePerformanceDates'); // Sort shows by date ascending
+    $programme2['performances'] = $performances;
+    $programmes[] = $programme2;
+  }
+  usort($programmes, 'compareShowDates'); // sort programmes by date of earliest performance
+  return $programmes;
+
+}
+
+// settings page stuff
+
+// register settings with Wordpress
+function oscarlistings_register_settings() {
+  add_option('oscarlistings_feedURL', '');
+  register_setting('oscarlistings_options_group', 'oscarlistings_feedURL', 'oscarlistings_callback');
+}
+add_action('admin_init', 'oscarlistings_register_settings');
+
+// register options page
+function oscarlistings_register_options_page() {
+  add_options_page('Oscar Listings', 'Oscar Listings', 'manage_options', 'oscarlistings', 'oscarlistings_options_page');
+}
+add_action('admin_menu', 'oscarlistings_register_options_page');
+
+// options page
+function oscarlistings_options_page() {
+  ?>
+  <div>
+    <?php screen_icon(); ?>
+    <h2>Oscar Listings Options</h2>
+    <form method="post" action="options.php">
+    <?php settings_fields( 'oscarlistings_options_group' ); ?>
+    <table>
+    <tr valign="top">
+    <th scope="row"><label for="oscarlistings_feedURL">Oscar API URL</label></th>
+    <td><input type="text" id="oscarlistings_feedURL" name="oscarlistings_feedURL" value="<?php echo get_option('oscarlistings_feedURL'); ?>" /></td>
+    </tr>
+    </table>
+    <?php  submit_button(); ?>
+    </form>
+  </div>
+  <?php
+}
+
+// function to compare performance dates (for use with usort)
+function comparePerformanceDates($a, $b) {
+	if ((string)$a['StartDate'] == (string)$b['StartDate']) {
+		if ((string)$a['StartTime'] == (string)$b['StartTime']) {
+			return 0;
+		}
+		$a_time = strtotime((string)$a['StartTime']);
+		$b_time = strtotime((string)$b['StartTime']);
+		return ($a_time < $b_time) ? -1 : 1;
+	}
+	$a_date = strtotime((string)$a['StartDate']);
+	$b_date = strtotime((string)$b['StartDate']);
+
+	return ($a_date < $b_date) ? -1 : 1;
+}
+
+// function to compare show dates for use with usort (note - assumes that performances are already sorted!)
+function compareShowDates($a, $b) {
+  return comparePerformanceDates($a['performances'][0], $b['performances'][0]);
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Oscar Listings Shortcodes ===
+Tags: oscar,savoy,savoysystems,theatre,listings,shows
+Requires at least: 5.0
+Tested up to: 5.2.2
+Requires PHP: 5.6
+License: GNU GPLv3
+
+Use WordPress shortcodes to create listings pages for shows using the Oscar booking system by Savoy Systems Ltd. Please note that this project is not affiliated with Savoy Systems Ltd.
+
+== Description ==
+Using a WordPress shortcode, this plugin enables you to create a listings page for shows pulled from the Oscar API. You can specify optional parameters in the shortcode to filter shows by parameters in the API.
+
+Please note that this project is not affiliated with Savoy Systems Ltd.
+
+== Installation ==
+#Configuring the API feed
+
+Once you have installed and activate the plugin, you need to specify the URL of your Oscar API feed. This can be done under Settings => Oscar Listings. The URL should look something like this:
+
+https://MyTheatre.savoysystems.co.uk/MyTheatre.dll/XMLPerformances?APIVersion=1
+
+#Creating a listings page
+
+To create a listings page, you will need to add the shortcode to a page.
+
+`[oscar-listings]` will display all the shows listed in the API
+
+You can specify more parameters to filter your results further.
+
+`[oscar-listings is_comedy=1]` will display all the shows listed in the API with the (custom) parameter `is_comedy` set to \"Y\".
+
+The use of multiple flags is treated as an inclusive OR. i.e.
+
+`[oscar-listings is_comedy=1 is_musical=1]` will display all the shows listed in the API with the (custom) parameter `is_comedy` set to \"Y\" AND all the shows listed in the API with the (custom) parameter `is_musical` set to \"Y\".
+
+*PLEASE NOTE* - shortcodes in WordPress are all run through strtolower(), therefore any flags in the Oscar API that have upper-case characters *will not work* with this plugin. Make sure the API flags you want to filter by are all lower-case.
+
+#Styling
+
+You can add styling to the listings page by customising your theme with additional CSS. In the WordPress back-end, go to Appearance => Customise => Additional CSS
+
+== Changelog ==
+1.0
+
+Initial release
